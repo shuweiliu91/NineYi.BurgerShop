4481fb4a0a421240d40fec765ededbd5ba890807
ShuWeiLiu <shuweiliu@nine-yi.com>
2017-10-30 18:08:09 +0800

    Add Simple Factory

---
+++ NineYi.BurgerShop/Factorys/SimpleFactory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using NineYi.BurgerShop.Breads;
+using NineYi.BurgerShop.Burgers;
+using NineYi.BurgerShop.Meats;
+using NineYi.BurgerShop.Veggies;
+
+
+namespace NineYi.BurgerShop.Factorys
+{
+    public class SimpleFactory
+    {
+        public static Burger CreateBurger(int shopChoice, int burgerChoice)
+        {
+            if (shopChoice == 1 && burgerChoice == 1)
+            {
+                return new TaipeiChickenBurger()
+                {
+                    //// 備料
+                    Bread = new WhiteBread(),
+                    Veggie = new Tomato(),
+                    Meat = new TaiwanChicken()
+                };
+            }
+            else if (shopChoice == 1 && burgerChoice == 2)
+            {
+                return new TaipeiPorkBurger()
+                {
+                    //// 備料
+                    Bread = new WhiteBread(),
+                    Veggie = new Tomato(),
+                    Meat = new Tenderloin()
+                };
+            }
+            else if (shopChoice == 2 && burgerChoice == 1)
+            {
+                return new NewYorkChickenBurger()
+                {
+                    //// 備料
+                    Bread = new WheatBread(),
+                    Veggie = new Onion(),
+                    Meat = new Turkey()
+                };
+            }
+            else if (shopChoice == 2 && burgerChoice == 2)
+            {
+                return new NewYorkPorkBurger()
+                {
+                    //// 備料
+                    Bread = new WheatBread(),
+                    Veggie = new Onion(),
+                    Meat = new Bacon()
+                };
+            }
+            else
+            {
+                throw new AggregateException("No such shop or burger!");
+            }
+        }
+    }
+}
--- NineYi.BurgerShop/Program.cs
+++ NineYi.BurgerShop/Program.cs
@@ -7,6 +7,7 @@ using NineYi.BurgerShop.Burgers;
 using NineYi.BurgerShop.Breads;
 using NineYi.BurgerShop.Veggies;
 using NineYi.BurgerShop.Meats;
+using NineYi.BurgerShop.Factorys;
 
 namespace NineYi.BurgerShop
 {
@@ -15,7 +16,6 @@ namespace NineYi.BurgerShop
         static void Main(string[] args)
         {
             //// 1. 使用者點餐
-
             Console.Write("Which shop do you like? (1)Taipei (2)NewYork: ");
             int shopChoice = int.Parse(Console.ReadLine());
 
@@ -23,48 +23,9 @@ namespace NineYi.BurgerShop
             int burgerChoice = int.Parse(Console.ReadLine());
 
             //// 2. 準備漢堡
-
-            Burger burger = null;
-
-            if (shopChoice == 1 && burgerChoice == 1)
-            {
-                burger = new TaipeiChickenBurger();
-
-                //// 備料
-                burger.Bread = new WhiteBread();
-                burger.Veggie = new Tomato();
-                burger.Meat = new TaiwanChicken();
-            }
-            else if (shopChoice == 1 && burgerChoice == 2)
-            {
-                burger = new TaipeiPorkBurger();
-
-                //// 備料
-                burger.Bread = new WhiteBread();
-                burger.Veggie = new Tomato();
-                burger.Meat = new Tenderloin();
-            }
-            else if (shopChoice == 2 && burgerChoice == 1)
-            {
-                burger = new NewYorkChickenBurger();
-
-                //// 備料
-                burger.Bread = new WheatBread();
-                burger.Veggie = new Onion();
-                burger.Meat = new Turkey();
-            }
-            else if(shopChoice == 2 && burgerChoice == 2)
-            {
-                burger = new NewYorkPorkBurger();
-
-                //// 備料
-                burger.Bread = new WheatBread();
-                burger.Veggie = new Onion();
-                burger.Meat = new Bacon();
-            }
+            Burger burger = SimpleFactory.CreateBurger(shopChoice, burgerChoice);
 
             //// 3. 烹飪漢堡
-
             burger.Cook();
         }
     }
